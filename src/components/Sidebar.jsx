1b3152450019512ce0623880ce361313a6ef3700
ahamefula nzubechi <nzube1999@gmail.com>
2024-10-10 09:13:43 -0700

    complete project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,20 +15,21 @@ const Sidebar = () => {
   const {isOpen, handleClose} = useContext(SidebarContext)
  
 
-  const {cart, clearCart} = useContext(CartContext)
+  const {cart, clearCart,total, itemAmount} = useContext(CartContext)
 
   return (
   <div className={`${isOpen?'right-0':'-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
     <div className='flex items-center justify-between py-6 border-b'>
       <div className='uppercase text-sm font-semibold '>
-        Shopping Bag(0)
+        Shopping Bag({itemAmount})
         </div>
       {/* icon */}
       <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
         <IoMdArrowForward className='text-2xl'/>
       </div>
     </div>
-    <div> {cart.map((item, id) =>{
+    <div className=' flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'> 
+      {cart.map((item, id) =>{
       return (
       <CartItem item={item} key={item.id}/>
       )
@@ -38,7 +39,7 @@ const Sidebar = () => {
       <div className='flex w-full justify-between items-center'>
         {/* total */}
         <div className='uppercase  font-semibold'>
-          <span className='mr-2'>Total:</span>$1000
+          <span className='mr-2'>Total:</span>${parseFloat(total).toFixed(2) }
         </div>
         {/* clear cart icon */}
         <div
@@ -47,6 +48,12 @@ const Sidebar = () => {
           <FiTrash2  />
         </div>
       </div>
+      <Link to='/' className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium'>
+      View cart
+      </Link>
+      <Link className='bg-primary flex p-4 justify-center items-center text-white w-full font-medium'>
+      Checkout
+      </Link>
     </div>
     </div>
     );
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -11,7 +11,34 @@ const CartProvider = ({children}) => {
 
   
   // cart state
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
+
+  // item amount state
+  const [itemAmount, setItemAmount] = useState(0);
+  // total price state
+  const[total, setTotal]=useState(0);
+
+  useEffect(() => {
+    const total = cart.reduce((accumulator, currentItem)=>{
+      return accumulator+currentItem.price * currentItem.amount
+    },0)
+  setTotal(total)
+    
+  })
+  
+
+  // update item amount
+useEffect(() => {
+  if(cart){
+    const amount = cart.reduce((accumulator, currentItem)=>{
+      return accumulator + currentItem.amount;
+    }, 0);
+    setItemAmount(amount);
+  }
+}, [cart])
+
+
+
   // add to cart
   const addToCart =(product,id)=> {
     const newItem = {...product, amount:1};
@@ -82,7 +109,18 @@ const CartProvider = ({children}) => {
   }
  
   return (
-    <CartContext.Provider value={{cart, addToCart, removeFromCart, clearCart, increaseAmount, decreaseAmount}}>
+    <CartContext.Provider
+     value={{
+      cart,
+      addToCart, 
+      removeFromCart, 
+      clearCart,
+      increaseAmount,
+      decreaseAmount,
+      itemAmount,
+      total
+      }}
+      >
       {children}
       </CartContext.Provider>
   )
